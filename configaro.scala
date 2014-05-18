11fb014e373b92a4d2413ad706bf7f3916143518
Erik Erlandson <eerlands@redhat.com>
2014-05-18 10:54:30 -0700

    experiment 2: ConfigurationValue is opaque with case classes underneath, using @specialized to handle generic implicit casting.  @specialized has problem that it is limited to AnyVal, doesn't handle AnyRef hierarchy

--- configaro.scala
+++ configaro.scala
@@ -1,23 +1,83 @@
 import scala.collection.mutable
 import scala.reflect.runtime.universe.{TypeTag, TypeRef, typeOf}
+import scala.language.implicitConversions
 
-// A function that also keeps its domain and range types as a payload
-class FunctionTP[D: TypeTag, R:TypeTag](f: D=>R) extends Function[D,R] {
-  val domainType = typeOf[D]
-  val rangeType = typeOf[R]
-  val func = f
-  override def apply(x: D):R = func(x)
-  def compose[E:TypeTag](g: E=>D):FunctionTP[E,R] = new FunctionTP(func compose g)
-  def andThen[S:TypeTag](g: R=>S):FunctionTP[D,S] = new FunctionTP(func andThen g)
+// 1st-class union types would be groovy here
+abstract class ConfiguredValue
+case class OptionLong(o:Option[Long]) extends ConfiguredValue
+case class OptionDouble(o:Option[Double]) extends ConfiguredValue
+case class OptionString(o:Option[String]) extends ConfiguredValue
+
+trait CVToValConverter[@specialized(Long, Int, Double, Float) T] {
+  def cast(cv:ConfiguredValue):T
+}
+class CVToLongConverter extends CVToValConverter[Long] {
+  def cast(cv:ConfiguredValue):Long = cv match {
+    case OptionLong(Some(v)) => v
+    case OptionDouble(Some(v)) if v == v.toLong.toDouble => v.toLong
+    case _ => throw new Exception
+  }
+}
+class CVToIntConverter extends CVToValConverter[Int] {
+  def cast(cv:ConfiguredValue):Int = cv match {
+    case OptionLong(Some(v)) if v == v.toInt.toLong => v.toInt
+    case OptionDouble(Some(v)) if v == v.toInt.toDouble => v.toInt
+    case _ => throw new Exception
+  }
+}
+class CVToDoubleConverter extends CVToValConverter[Double] {
+  def cast(cv:ConfiguredValue):Double = cv match {
+    case OptionLong(Some(v)) => v.toDouble
+    case OptionDouble(Some(v)) => v
+    case _ => throw new Exception
+  }
+}
+class CVToFloatConverter extends CVToValConverter[Float] {
+  def cast(cv:ConfiguredValue):Float = cv match {
+    case OptionLong(Some(v)) => v.toFloat
+    case OptionDouble(Some(v)) => v.toFloat
+    case _ => throw new Exception
+  }
 }
 
-// Generates a function that applies a function 'f'
-// on payload of an Option[_], -- returns None if arg is None, or if
-// f threw an exception
-// (might be typically used for type casting from string to other types)
-def typedPred[D:TypeTag,R:TypeTag](f:D=>R):FunctionTP[Option[D],Option[R]] = {
-  new FunctionTP((d:Option[D]) => {
-    val r:Option[R] = { d match {
+object CVImplicits {
+  // implicit import of option values into ConfiguredValue
+  implicit def OptionToCV[T:TypeTag](o:Option[T]):ConfiguredValue = {
+    val typeOptionLong = typeOf[Option[Long]]
+    val typeOptionInt = typeOf[Option[Int]]
+    val typeOptionDouble = typeOf[Option[Double]]
+    val typeOptionFloat = typeOf[Option[Float]]
+    val typeOptionString = typeOf[Option[String]]
+    typeOf[Option[T]] match {
+      case `typeOptionLong` => OptionLong(o.asInstanceOf[Option[Long]])
+      case `typeOptionDouble` => OptionDouble(o.asInstanceOf[Option[Double]])
+      case `typeOptionString` => OptionString(o.asInstanceOf[Option[String]])
+      case `typeOptionInt` => OptionLong(o.asInstanceOf[Option[Int]].map(_.toLong))
+      case `typeOptionFloat` => OptionDouble(o.asInstanceOf[Option[Float]].map(_.toDouble))
+      case _ => throw new Exception
+    }
+  }
+
+  implicit val CVToLong = new CVToLongConverter
+  implicit val CVToInt = new CVToIntConverter
+  implicit val CVToDouble = new CVToDoubleConverter
+  implicit val CVToFloat = new CVToFloatConverter
+
+  implicit def CVToString(cv: ConfiguredValue):String = {
+    cv match {
+      case OptionLong(Some(v)) => v.toString
+      case OptionDouble(Some(v)) => v.toString
+      case OptionString(Some(v)) => v
+      case _ => throw new Exception
+    }
+  }
+}
+
+import CVImplicits._
+
+def castPred[R:TypeTag](f:String=>R):Function[Option[String],ConfiguredValue] = {
+  (d:Option[String]) => {
+    val r:Option[R] = d match {
       case Some(v) => {
         try {
           Some(f(v))
@@ -26,35 +86,84 @@ def typedPred[D:TypeTag,R:TypeTag](f:D=>R):FunctionTP[Option[D],Option[R]] = {
         }
       } 
       case None => None
-    }}
-    r
-  })
-}
-
-
-def opLT[T](a:T, b:T)(implicit n: Numeric[T]):Boolean = { n.lt(a,b) }
-
-def numPred[D: Numeric](t:D, test:(D,D)=>Boolean):Function[Option[D],Option[D]] = {
-  (d:Option[D]) => {
-    val r:Option[D] = d match {
-      case Some(v) => {
-        if (test(v, t)) Some(v)
-        else None
-      }
-      case None => None
     }
-    r
+    // leverage implicit casting functions here:
+    val cv:ConfiguredValue = r
+    cv
   }
 }
 
 // type checking and casting
-val isInt = typedPred((x:String)=>x.toInt)
-val isLong = typedPred((x:String)=>x.toLong)
-val isDouble = typedPred((x:String)=>x.toDouble)
-val isString = typedPred((x:String)=>x)
+val isLong = castPred((x:String)=>x.toLong)
+val isDouble = castPred((x:String)=>x.toDouble)
+val isString = castPred((x:String)=>x)
+
+def test[@specialized(Long,Int,Double,Float)T:CVToValConverter](cv:ConfiguredValue):T = {
+  val caster = implicitly[CVToValConverter[T]]
+  caster.cast(cv)
+}
+
+def opLT[T](a:T, b:T)(implicit n: Numeric[T]):Boolean = { n.lt(a,b) }
+
+def numPred[@specialized(Long,Double,Int,Float)D: CVToValConverter :TypeTag](t:D, test:(D,D)=>Boolean):Function[ConfiguredValue,ConfiguredValue] = {
+  (d:ConfiguredValue) => {
+    val caster = implicitly[CVToValConverter[D]]
+    val r:Option[D] = try {
+      val v:D = caster.cast(d)
+      if (test(v, t)) Some(v)
+      else None
+    } catch {
+      case _ : Throwable => None
+    }
+    val cv: ConfiguredValue = r
+    cv
+  }
+}
 
 // boundary checking
-def isLT[D:Numeric](t:D):Function[Option[D],Option[D]] = numPred(t, (vv:D,tt:D)=>opLT(vv,tt))
+def isLT[D :CVToValConverter :Numeric :TypeTag](t:D):Function[ConfiguredValue,ConfiguredValue] = numPred(t, (vv:D,tt:D)=>opLT(vv,tt))
+
+
+// holds a configuration, and imports configuration requirements
+// (requirements are defined below)
+class Config(metaConfig: Map[String, Option[String] => ConfiguredValue] = Map()) {
+  val conf: mutable.Map[String, String] = mutable.Map()
+
+  def put[T](s:String, v:T, check:Boolean=true):Unit = {
+    conf.put(s, v.toString)
+    if (check) conf.get(s)
+  }
+
+  def get(s:String):ConfiguredValue = {
+    val f = metaConfig.getOrElse(s, isString)
+    f(conf.get(s))
+  }
+
+/*
+  def getAs[T:TypeTag](s:String):T = {
+    val f = metaConfig.getOrElse(s, isString)
+    val r:T = f(conf.get(s))
+    r
+  }
+*/
+}
+
+// here is where you define policies for each config variable
+// this could reside in its own file for easy maintenance
+val reqmap:Map[String, Option[String] => ConfiguredValue] = Map(
+  "L" -> (isLong andThen isLT(10)),
+  "D" -> (isDouble),
+  "S" -> (isString)
+)
+
+val conf = new Config(reqmap)
+conf.put("L", 42)
+conf.put("D", 3.14)
+conf.put("S", "octocat")
+
+/*
+
+
 def isLE[D:Numeric](t:D):Function[Option[D],Option[D]] = numPred(t, (vv:D,tt:D)=>(!opLT(tt,vv)))
 def isGT[D:Numeric](t:D):Function[Option[D],Option[D]] = numPred(t, (vv:D,tt:D)=>opLT(tt,vv))
 def isGE[D:Numeric](t:D):Function[Option[D],Option[D]] = numPred(t, (vv:D,tt:D)=>(!opLT(vv,tt)))
@@ -72,43 +181,4 @@ def defaultTo[D](dv:D):Function[Option[D],Option[D]] = {
     }
   }
 }
-
-// holds a configuration, and imports configuration requirements
-// (requirements are defined below)
-class Config extends ConfigReqs {
-  val conf: mutable.Map[String, String] = mutable.Map()
-
-  def put[T](s:String, v:T, check:Boolean=true):Unit = {
-    conf.put(s, v.toString)
-    if (check) conf.get(s)
-  }
-
-  def get(s:String):Any = {
-    val f = reqmap.getOrElse(s, isString)
-    f(conf.get(s))
-  }
-}
-
-// here is where you define policies for each config variable
-// this could reside in its own file for easy maintenance
-trait ConfigReqs {
-  val reqmap:Map[String, FunctionTP[Option[String], _]] = Map(
-    // an integer, >= 10
-    "a" -> (isInt andThen isGE(10) andThen defaultTo(10)),
-
-    // a double, > 0.0 and < 1.0
-    "b" -> (isDouble andThen isGT(0.0) andThen isLT(1.0) andThen defaultTo(0.75)),
-
-    // a long >= 0 and <= 100
-    "c" -> (isLong andThen isOnClosed(0L, 100L) andThen defaultTo(50L)),
-
-    // a string, default to "foo"
-    "z" -> (isString andThen defaultTo("foo"))
-  )
-}
-
-val conf = new Config
-conf.put("a", 15)
-println(conf.get("a"))
-conf.put("a", 5)
-println(conf.get("a"))
+*/
