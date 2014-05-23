4f4a87460e0a8ef4b16b96bd73d14656a0ee8c7e
Erik Erlandson <eerlands@redhat.com>
2014-05-23 16:41:39 -0700

    folded dsl work back into configaro.scala

--- configaro.scala
+++ configaro.scala
@@ -18,200 +18,232 @@ limitations under the License.
 
 import scala.collection.mutable
 import scala.language.implicitConversions
+import scala.language.postfixOps
 
-// a function that supports "|" operator as shorthand for andThen
-class PF[D,R](f:D=>R) extends Function[D,R] {
-  val func = f
-  def apply(d:D):R = func(d)
-  def |[S](g:R=>S):PF[D,S] = new PF(func andThen g)
-}
-
-// a configured value, for supporting type conversions
-class CV[+T](op:Option[T]) {
-  val o = op
-  def isNone:Boolean = o match {
-    case None => true
-    case _ => false
-  }
-}
 
 case class ConfigaroConversionException(message:String) extends Exception(message)
+case class PolicyViolation(msg:String) extends Exception(msg)
 
-def conversionExceptionMessage[T](o: Option[T]):String = {
-  val (tString, vString) = o match {
-    case Some(v) => (v.getClass.getName, v.toString) 
-    case None => ("None", "None")
-  }
-  s"Failed to convert CV[$tString] = $vString to the requested type"
+
+def conversionExceptionMessage(v:Any):String = {
+  val tString = v.getClass.getName
+  s"Failed to convert $tString = $v to the requested type"
 }
 
-trait ConvertCVToV[V] {
-  def convert(cv:CV[Any]):V
+trait ConvertOptionToV[V] {
+  def convert(cv:Any):V
 }
 
-object ConvertCVToVObjects {
-  implicit val objectCVToLong = new ConvertCVToV[Long] {
-    def convert(cv:CV[Any]):Long = cv.o match {
-      case Some(v: Long) => v
-      case Some(v: Double) if v == v.toLong.toDouble => v.toLong
-      case _ => throw new ConfigaroConversionException(conversionExceptionMessage(cv.o))
-    }
-  }
-  implicit val objectCVToInt = new ConvertCVToV[Int] {
-    def convert(cv:CV[Any]):Int = cv.o match {
-      case Some(v: Long) if v == v.toInt.toLong => v.toInt
-      case Some(v: Double) if v == v.toInt.toDouble => v.toInt
-      case _ => throw new ConfigaroConversionException(conversionExceptionMessage(cv.o))
-    }
-  }
-  implicit val objectCVToDouble = new ConvertCVToV[Double] {
-    def convert(cv:CV[Any]):Double = cv.o match {
-      case Some(v: Long) => v.toDouble
-      case Some(v:Double) => v
-      case _ => throw new ConfigaroConversionException(conversionExceptionMessage(cv.o))
-    }
-  }
-  implicit val objectCVToFloat = new ConvertCVToV[Float] {
-    def convert(cv:CV[Any]):Float = cv.o match {
-      case Some(v: Long) => v.toFloat
-      case Some(v:Double) => v.toFloat
-      case _ => throw new ConfigaroConversionException(conversionExceptionMessage(cv.o))
-    }
-  }
-  implicit val objectCVToString = new ConvertCVToV[String] {
-    def convert(cv:CV[Any]):String = cv.o match {
-      case Some(v) => v.toString
-      case _ => throw new ConfigaroConversionException(conversionExceptionMessage(cv.o))
-    }
-  }
-}
-
-import ConvertCVToVObjects._
-
-def convertCVToV[V:ConvertCVToV](cv:CV[Any]):V = {
-  val conv = implicitly[ConvertCVToV[V]]
-  conv.convert(cv)
-}
-
-object Convert {
-/*
-  Scala does not seem to handle generic implicit conversion return types.
-  It compiles, but it never matches at run time
-  implicit def convertCVToV[V:ConvertCVToV](cv:CV[Any]):V = {
-    val conv = implicitly[ConvertCVToV[V]]
-    conv.convert(cv)
-  }
-*/
-
-  implicit def convertCVToLong(cv:CV[Any]):Long = objectCVToLong.convert(cv)
-  implicit def convertCVToInt(cv:CV[Any]):Int = objectCVToInt.convert(cv)
-  implicit def convertCVToDouble(cv:CV[Any]):Double = objectCVToDouble.convert(cv)
-  implicit def convertCVToFloat(cv:CV[Any]):Float = objectCVToFloat.convert(cv)
-  implicit def convertCVToString(cv:CV[Any]):String = objectCVToString.convert(cv)
-}
-import Convert._
-
-
-// Generates a function that applies a function 'f' to option payloads
-def typedPred[D,R](f:D=>R):PF[Option[D],CV[R]] = {
-  new PF((d:Option[D]) => new CV(try {
-    d match {
-      case Some(v) => Some(f(v))
-      case None => throw new Exception
+implicit val objectOptionToLong = new ConvertOptionToV[Long] {
+  def convert(cv:Any):Long = try { cv match {
+      case v: Long => v
+      case v: Double if v == v.toLong.toDouble => v.toLong
+      case v: String => v.toLong
+      case _ => throw new Exception
     }
   } catch {
-    // support alternate failure response policies here
-    case _ : Throwable => None
-  }))
+    case _: Throwable => throw new ConfigaroConversionException(conversionExceptionMessage(cv))
+  }
 }
-
-// default assignment
-def defaultTo[D](dv:D):Function[CV[D],CV[D]] = {
-  (d:CV[D]) => new CV(d.o match {
-      case Some(v) => Some(v)
-      case None => Some(dv)
+implicit val objectOptionToInt = new ConvertOptionToV[Int] {
+  def convert(cv:Any):Int = try { cv match {
+      case v: Long if v == v.toInt.toLong => v.toInt
+      case v: Double if v == v.toInt.toDouble => v.toInt
+      case v: String => v.toInt
+      case _ => throw new Exception
     }
-  )
+  } catch {
+    case _: Throwable => throw new ConfigaroConversionException(conversionExceptionMessage(cv))
+  }  
+}
+implicit val objectOptionToDouble = new ConvertOptionToV[Double] {
+  def convert(cv:Any):Double = try { cv match {
+      case v: Long => v.toDouble
+      case v: Double => v
+      case v: String => v.toDouble
+      case _ => throw new Exception
+    }
+  } catch {
+    case _: Throwable => throw new ConfigaroConversionException(conversionExceptionMessage(cv))
+  }
+}
+implicit val objectOptionToFloat = new ConvertOptionToV[Float] {
+  def convert(cv:Any):Float = try { cv match {
+      case v: Long => v.toFloat
+      case v: Double => v.toFloat
+      case v: String => v.toFloat
+      case _ => throw new Exception
+    }
+  } catch {
+    case _: Throwable => throw new ConfigaroConversionException(conversionExceptionMessage(cv))
+  }
+}
+implicit val objectOptionToString = new ConvertOptionToV[String] {
+  def convert(cv:Any):String = try {
+    cv.toString
+  } catch {
+    case _: Throwable => throw new ConfigaroConversionException(conversionExceptionMessage(cv))
+  }
 }
 
-def opLT[T](a:T, b:T)(implicit n: Numeric[T]):Boolean = { n.lt(a,b) }
 
-def numTestPred[D:Numeric](t:D, test:(D,D)=>Boolean):(CV[D]=>CV[D]) = {
-  (d:CV[D]) => new CV(
-    try {
-      d.o match {
-        case Some(v) if (test(v, t)) => Some(v)
-        case _ => throw new Exception
-      }
+trait MetaConfiguration {
+  val policy = this
+
+  val map: mutable.Map[String, Function[Option[String], Option[Any]]] = mutable.Map()
+
+  trait TypeConverter[T] {
+    def func: String => T
+  }
+
+  implicit val tcLong = new TypeConverter[Long] {
+    def func = (s:String) => try { s.toLong } catch { case _ :Throwable => throw new PolicyViolation(conversionExceptionMessage(s)) }
+  }
+  implicit val tcInt = new TypeConverter[Int] {
+    def func = (s:String) => try { s.toInt } catch { case _ :Throwable => throw new PolicyViolation(conversionExceptionMessage(s)) }
+  }
+  implicit val tcDouble = new TypeConverter[Double] {
+    def func = (s:String) => try { s.toDouble } catch { case _ :Throwable => throw new PolicyViolation(conversionExceptionMessage(s)) }
+  }
+  implicit val tcFloat = new TypeConverter[Float] {
+    def func = (s:String) => try { s.toFloat } catch { case _ :Throwable => throw new PolicyViolation(conversionExceptionMessage(s)) }
+  }
+  implicit val tcString = new TypeConverter[String] {
+    def func = (s:String) => try { s.toString } catch { case _ :Throwable => throw new PolicyViolation(conversionExceptionMessage(s)) }
+  }
+
+  private var enforceLevelGlobal = 'none
+
+  private def enforceCheck(level:Symbol):Unit = {
+    level match {
+      case 'none | 'warn | 'error => return
+      case _ => throw new Exception("undefined enforce level "+level)
+    }
+  }
+
+  def enforce(level:Symbol):Unit = {
+    enforceCheck(level)
+    enforceLevelGlobal = level
+  }
+
+  private def wrap[E,S](g:E=>S, level:Symbol):(Option[E]=>Option[S]) = {
+    enforceCheck(level)
+    (d:Option[E]) => try {
+      d.map(g)
     } catch {
-      // support alternate failure response policies here
-      case _: Throwable => None
+      case PolicyViolation(msg) => level match {
+        case 'none => None
+        case 'warn => {
+          // generalized logging goes here
+          System.err.println("WARNING: "+msg)
+          None
+        }
+        case 'error | _  => {
+          // generalized logging goes here
+          System.err.println("ERROR: "+msg)
+          throw new PolicyViolation(msg)
+        }
+      }
     }
-  )
+  }
+
+  def tpe[T:TypeConverter]:(String=>T) = implicitly[TypeConverter[T]].func
+
+  def opLT[T](a:T, b:T)(implicit n: Numeric[T]):Boolean = { n.lt(a,b) }
+
+  class Context[R](k:String, f:Option[String]=>Option[R], level:Symbol) {
+    private val key = k
+    private val func = f
+    private var enforceLevel = level
+
+    private def comp[S>:R,T](g:Option[S]=>Option[T]):Context[T] = {
+      val h = func andThen g
+      map(key) = h
+      new Context(key, h, enforceLevel)
+    }
+
+    // configures scoped to current parameter context
+    def enforce(lev:Symbol):Context[R] = {
+      enforceCheck(lev)
+      enforceLevel = lev
+      this
+    }
+
+    // pipe in a new function, automatically wrapped in 
+    // Option, violation and logging boilerplate
+    def pipe[S>:R,T](g:S=>T):Context[T] = comp(wrap(g, enforceLevel))
+
+    // for accepting type conversion/constraint predicates
+    // i.e.  is tpe[Long]
+    def is[S](g:R=>S):Context[S] = pipe(g)
+
+    // replace any missing value (None) with a default
+    def default(dv:R):Context[R] = comp(
+      (d:Option[R]) => d match {
+        case Some(v) => Some(v)
+        case None => Some(dv)
+      })
+
+    // boundary checking
+    def lt[D >: R :Numeric](t:D):Context[D] = pipe((x:D) => {
+      if (!opLT(x,t)) throw PolicyViolation("$x >= $t")
+      x})
+    def le[D >: R :Numeric](t:D):Context[D] = pipe((x:D) => {
+      if (opLT(t,x)) throw PolicyViolation("$x > $t")
+      x})
+    def gt[D >: R :Numeric](t:D):Context[D] = pipe((x:D) => {
+      if (!opLT(t,x)) throw PolicyViolation("$x <= $t")
+      x})
+    def ge[D >: R :Numeric](t:D):Context[D] = pipe((x:D) => {
+      if (opLT(x,t)) throw PolicyViolation("$x < $t")
+      x})
+  }
+
+  implicit def handleString(s:String):Context[String] = {
+    map -= s
+    new Context(s, (x:Option[String])=>x, enforceLevelGlobal)
+  }
 }
-
-// type checking and casting
-// ignoring Int and Float as internal types results in
-// shorter case statements for internal conversions, however
-// it might be worth adding them in, particularly Int, since
-// you can say defaultTo(7) instead of defaultTo(7L)
-val isLong = typedPred((x:String)=>x.toLong)
-val isDouble = typedPred((x:String)=>x.toDouble)
-val isString = typedPred((x:String)=>x)
-
-// boundary checking
-def isLT[D:Numeric](t:D):(CV[D]=>CV[D]) = numTestPred(t, (vv:D,tt:D)=>opLT(vv,tt))
-def isLE[D:Numeric](t:D):(CV[D]=>CV[D]) = numTestPred(t, (vv:D,tt:D)=>(!opLT(tt,vv)))
-def isGT[D:Numeric](t:D):(CV[D]=>CV[D]) = numTestPred(t, (vv:D,tt:D)=>opLT(tt,vv))
-def isGE[D:Numeric](t:D):(CV[D]=>CV[D]) = numTestPred(t, (vv:D,tt:D)=>(!opLT(vv,tt)))
-
-type MetaConfiguration = Map[String, Function[Option[String], CV[_]]]
 
 // holds a configuration, and imports configuration requirements
 // (requirements are defined below)
-class Config(mc: MetaConfiguration) extends Function[String, CV[Any]] {
-  val metaConfig = mc
+class Config(mc: MetaConfiguration) extends Function[String, Option[Any]] {
+  val metaConfig = mc.map
   val conf: mutable.Map[String, String] = mutable.Map()
 
-  def apply(s:String):CV[Any] = {
-    metaConfig.getOrElse(s, isString)(conf.get(s))
+  def apply(s:String):Option[Any] = {
+    metaConfig.getOrElse(s, (x:Option[String])=>x)(conf.get(s))
   }
 
   def put[T](s:String, v:T, check:Boolean=true):Unit = {
     conf.put(s, v.toString)
   }
 
-  def get[T:ConvertCVToV](s:String):Option[T] = {
-    try {
-      val conv = implicitly[ConvertCVToV[T]]
-      val cv = this(s)
-      if (cv.isNone) None else Some(conv.convert(cv))
-    } catch {
-      case _: Throwable => None
-    }
+  def get[T:ConvertOptionToV](s:String):Option[T] = {
+    this(s).map(implicitly[ConvertOptionToV[T]].convert)
   }
 
-  def getOrElse[T:ConvertCVToV](s:String, d:T):T = {
+  def getOrElse[T:ConvertOptionToV](s:String, d:T):T = {
     get[T](s).getOrElse(d)
   }
 
-  def require[T:ConvertCVToV](s:String):T = {
-    val conv = implicitly[ConvertCVToV[T]]
-    conv.convert(this(s))
+  def require[T:ConvertOptionToV](s:String):T = {
+    this(s) match {
+      case Some(v) => implicitly[ConvertOptionToV[T]].convert(v)
+      case None => throw new ConfigaroConversionException(conversionExceptionMessage(None))
+    }
   }
 }
 
 // here is where you define policies for each config variable
 // this could reside in its own file for easy maintenance
-val metaConfigExample:MetaConfiguration = Map(
-  "a" -> (isLong | isGE(0L) | defaultTo(42L)),
- 
-  "b" -> (isDouble | isGE(0.0) | isLT(6.28) | defaultTo(3.14)),
-
-  "c" -> (isLong | isGT(0L) | defaultTo(1L)),
-
-  "z" -> (isString | defaultTo("wowbagger"))
-)
+object metaConfigExample extends MetaConfiguration {
+  "a" is tpe[Long] default 42L
+  "b" is tpe[Int] default -1
+  "radians" is tpe[Double] default 3.1415 ge 0.0 lt 6.2830
+  "age" is tpe[Int] default 45 ge 0 le 150
+  "name" is tpe[String] default "wowbagger"
+}
 
 val conf = new Config(metaConfigExample)
 
@@ -226,5 +258,4 @@ assert(conf.require[Int]("a") == 7)
 
 conf.put("q", "foo")
 assert(conf.get[String]("q") == Some("foo"))
-assert(conf.get[Int]("q") == None)
-assert(conf.getOrElse[Int]("q", 77) == 77)
+assert(conf.require[String]("q") == "foo")
