86b4a373c48fa5b500c57b870382ed724b034f49
Erik Erlandson <eerlands@redhat.com>
2014-05-29 15:48:12 -0700

    regex test

--- src/test/scala/com/manyangled/configaro/PropertyPolicySuite.scala
+++ src/test/scala/com/manyangled/configaro/PropertyPolicySuite.scala
@@ -336,4 +336,17 @@ class PropertyPolicySuite extends FunSuite {
     assert(policy("a")(Some("9")) == Some(9.0))
     assert(policy("a")(Some("x")) == None)
   }
+
+  test("regex") {
+    object policy extends PropertyPolicy {
+      "a" regex """^[a-z]+$"""
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("")) == None)
+    assert(policy("a")(Some("q")) == Some("q"))
+    assert(policy("a")(Some("qr")) == Some("qr"))
+    assert(policy("a")(Some("Q")) == None)
+    assert(policy("a")(Some("qR")) == None)
+    assert(policy("a")(Some("Qr")) == None)
+  }
 }
