5ba46af871eeb74eb8dac8d9be9e8e9d457a8e3d
Erik Erlandson <eerlands@redhat.com>
2014-05-29 15:35:42 -0700

    bound checking tests

--- src/test/scala/com/manyangled/configaro/PropertyPolicySuite.scala
+++ src/test/scala/com/manyangled/configaro/PropertyPolicySuite.scala
@@ -128,4 +128,212 @@ class PropertyPolicySuite extends FunSuite {
     assert(policy("a")(Some("3")) == Some(3.0))
     assert(policy("a")(Some("x")) == Some(7.0))
   }
+
+  test("bound lt Int") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Int]   lt 7
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5))
+    assert(policy("a")(Some("6")) == Some(6))
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound le Int") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Int]   le 7
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5))
+    assert(policy("a")(Some("6")) == Some(6))
+    assert(policy("a")(Some("7")) == Some(7))
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound gt Int") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Int]   gt 7
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == Some(8))
+    assert(policy("a")(Some("9")) == Some(9))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound ge Int") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Int]   ge 7
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == Some(7))
+    assert(policy("a")(Some("8")) == Some(8))
+    assert(policy("a")(Some("9")) == Some(9))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound lt Long") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Long]   lt 7L
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5L))
+    assert(policy("a")(Some("6")) == Some(6L))
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound le Long") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Long]   le 7L
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5L))
+    assert(policy("a")(Some("6")) == Some(6L))
+    assert(policy("a")(Some("7")) == Some(7L))
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound gt Long") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Long]   gt 7L
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == Some(8L))
+    assert(policy("a")(Some("9")) == Some(9L))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound ge Long") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Long]   ge 7L
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == Some(7L))
+    assert(policy("a")(Some("8")) == Some(8L))
+    assert(policy("a")(Some("9")) == Some(9L))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound lt Float") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Float]   lt 7f
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5f))
+    assert(policy("a")(Some("6")) == Some(6f))
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound le Float") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Float]   le 7f
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5f))
+    assert(policy("a")(Some("6")) == Some(6f))
+    assert(policy("a")(Some("7")) == Some(7f))
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound gt Float") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Float]   gt 7f
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == Some(8f))
+    assert(policy("a")(Some("9")) == Some(9f))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound ge Float") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Float]   ge 7f
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == Some(7f))
+    assert(policy("a")(Some("8")) == Some(8f))
+    assert(policy("a")(Some("9")) == Some(9f))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound lt Double") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Double]   lt 7.0
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5.0))
+    assert(policy("a")(Some("6")) == Some(6.0))
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound le Double") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Double]   le 7.0
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == Some(5.0))
+    assert(policy("a")(Some("6")) == Some(6.0))
+    assert(policy("a")(Some("7")) == Some(7.0))
+    assert(policy("a")(Some("8")) == None)
+    assert(policy("a")(Some("9")) == None)
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound gt Double") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Double]   gt 7.0
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == None)
+    assert(policy("a")(Some("8")) == Some(8.0))
+    assert(policy("a")(Some("9")) == Some(9.0))
+    assert(policy("a")(Some("x")) == None)
+  }
+
+  test("bound ge Double") {
+    object policy extends PropertyPolicy {
+      "a" is tpe[Double]   ge 7.0
+    }
+    assert(policy("a")(None) == None)
+    assert(policy("a")(Some("5")) == None)
+    assert(policy("a")(Some("6")) == None)
+    assert(policy("a")(Some("7")) == Some(7.0))
+    assert(policy("a")(Some("8")) == Some(8.0))
+    assert(policy("a")(Some("9")) == Some(9.0))
+    assert(policy("a")(Some("x")) == None)
+  }
 }
